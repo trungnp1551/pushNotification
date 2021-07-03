8aa607cc2849a0f34e0345c65aed41c8c55d227d
Trun <trungnq15501@gmail.com>
2021-07-03 09:16:02 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,17 @@ import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
+// ignore: missing_return
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
   if (message.containsKey('data')) {
     // Handle data message
+    // ignore: unused_local_variable
     final dynamic data = message['data'];
   }
 
   if (message.containsKey('notification')) {
     // Handle notification message
+    // ignore: unused_local_variable
     final dynamic notification = message['notification'];
   }
 
@@ -113,6 +116,7 @@ class _DetailPageState extends State<DetailPage> {
                       child: Column(
                         children: <Widget>[
                           Text('Today match:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                          // ignore: deprecated_member_use
                           Text( _item.matchteam, style: Theme.of(context).textTheme.title)
                         ],
                       ),
@@ -122,6 +126,7 @@ class _DetailPageState extends State<DetailPage> {
                       child: Column(
                         children: <Widget>[
                           Text('Score:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                          // ignore: deprecated_member_use
                           Text( _item.score, style: Theme.of(context).textTheme.title)
                         ],
                       ),
@@ -143,15 +148,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -162,15 +158,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -178,9 +165,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ignore: unused_field
   bool _topicButtonsDisabled = false;
 
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  // ignore: unused_field
   final TextEditingController _topicController =
   TextEditingController(text: 'topic');
 
@@ -188,12 +177,14 @@ class _MyHomePageState extends State<MyHomePage> {
     return AlertDialog(
       content: Text("${item.matchteam} with score: ${item.score}"),
       actions: <Widget>[
+        // ignore: deprecated_member_use
         FlatButton(
           child: const Text('CLOSE'),
           onPressed: () {
             Navigator.pop(context, false);
           },
         ),
+        // ignore: deprecated_member_use
         FlatButton(
           child: const Text('SHOW'),
           onPressed: () {
@@ -258,12 +249,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -283,6 +269,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Column(
                       children: <Widget>[
                         Text('Welcome to this Flutter App:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                        // ignore: deprecated_member_use
                         Text('You already subscribe to the matchscore topic', style: Theme.of(context).textTheme.title)
                       ],
                     ),
